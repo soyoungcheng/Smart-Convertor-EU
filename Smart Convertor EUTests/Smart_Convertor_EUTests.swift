333a243c3e529a59e32f73036068c499ea6333b5
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-09-06 15:31:13 +0200

    Initial Commit

---
+++ Smart Convertor EUTests/Smart_Convertor_EUTests.swift
@@ -0,0 +1,34 @@
+//
+//  Smart_Convertor_EUTests.swift
+//  Smart Convertor EUTests
+//
+//  Created by Uladzislau Daratsiuk on 9/6/18.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import XCTest
+@testable import Smart_Convertor_EU
+
+class Smart_Convertor_EUTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
