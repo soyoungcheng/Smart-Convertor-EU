3cf679624182616b25349674631812a587f68b3f
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-09-09 18:11:06 +0200

    commit

--- Smart Convertor EU/Controllers/AboutVC.swift
+++ Smart Convertor EU/Controllers/AboutVC.swift
@@ -12,18 +12,30 @@ class AboutVC: UIViewController {
     
     let mainView = ShadowView()
     let developerInfoLabel = MainLabel(text: "Developed by IOCode, iOS Developer Uladzislau Daratsiuk", textAligment: .center, numberOfLines: 3)
+    let infoLabel = MainLabel(text: "All currency data is sourced from the European Central Bank and updated daily at around 4:00pm CET.", textAligment: .center, numberOfLines: 5)
     let emailButton = ActionButton()
+    let privacyButton = MainButton(text: "Privacy Policy")
+    
     
     fileprivate func setupVIew(){
         view.addSubview(mainView)
         mainView.addSubview(developerInfoLabel)
+        mainView.addSubview(infoLabel)
         mainView.addSubview(emailButton)
+        mainView.addSubview(privacyButton)
         
         developerInfoLabel.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 20).isActive = true
         developerInfoLabel.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
         developerInfoLabel.leftAnchor.constraint(equalTo: mainView.leftAnchor, constant: 20).isActive = true
         developerInfoLabel.rightAnchor.constraint(equalTo: mainView.rightAnchor, constant: -20).isActive = true
         developerInfoLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        
+        infoLabel.topAnchor.constraint(equalTo: developerInfoLabel.bottomAnchor, constant: 20).isActive = true
+        infoLabel.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
+        infoLabel.leftAnchor.constraint(equalTo: mainView.leftAnchor, constant: 20).isActive = true
+        infoLabel.rightAnchor.constraint(equalTo: mainView.rightAnchor, constant: -20).isActive = true
+        infoLabel.bottomAnchor.constraint(equalTo: emailButton.topAnchor, constant: -40).isActive = true
         
         emailButton.setImage(UIImage(named: "email"), for: .normal)
         emailButton.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
@@ -38,6 +50,13 @@ class AboutVC: UIViewController {
         mainView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         mainView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
         mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+        privacyButton.addTarget(self, action: #selector(handlePrivacy), for: .touchUpInside)
+        privacyButton.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: -10).isActive = true
+        privacyButton.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
+        privacyButton.leftAnchor.constraint(equalTo: mainView.leftAnchor, constant: 20).isActive = true
+        privacyButton.rightAnchor.constraint(equalTo: mainView.rightAnchor, constant: -20).isActive = true
+        privacyButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
     }
     
     fileprivate func setupNavBar(){
--- Smart Convertor EU/Handlers/AboutHandlers.swift
+++ Smart Convertor EU/Handlers/AboutHandlers.swift
@@ -11,6 +11,17 @@ import UIKit
 
 extension AboutVC {
     @objc public func handleShare(){
-        
+        let mainVC = UIActivityViewController(activityItems: ["Take a look at this amazing app. which can convert different currencies, it's called 'Smart Currency'"], applicationActivities: nil)
+        mainVC.popoverPresentationController?.sourceView = self.view
+        self.present(mainVC, animated: true, completion: nil)
+    }
+    
+    @objc public func handlePrivacy(){
+        guard let url = URL(string: "https://github.com/ula1990/Smart-Convertor-EU/blob/master/README.md") else {return}
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
     }
 }
